9ef9f674292dd821b3e836cff063c6d0cc913972
Abhi <abhilasha.reva61@gmail.com>
2023-12-18 12:37:25 +0100

    send to netifly

--- src/components/Components/Deletetask.jsx
+++ src/components/Components/Deletetask.jsx
@@ -4,8 +4,9 @@ import { useAppContext } from '../context/Appcontext';
 function Deletetask({id}) {
     const {deleteTodo} = useAppContext();
   return (
-    <div>
-       <button onClick={() => {deleteTodo(id)}} className="btn font-bold md:text-[1.2vw] text-[4vw] text-yellow-950  rounded-2 absolute top-5 right-5 bg-white border-amber-500">X</button></div>
+    <div> 
+      <h1 className=' text text-center  font-bold'>ON Development Phase</h1>
+      <button onClick={() => {deleteTodo(id)}} className="btn font-bold md:text-[1.2vw] text-[4vw] text-yellow-950  rounded-2 absolute top-5 right-5 bg-white border-amber-500">X</button></div>
   )
 }
   
